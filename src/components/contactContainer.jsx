2b1068890bd5fdc2f628566d900bfdf23d5c2af9
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-02-19 21:02:00 -0600

    adding more contact info

--- src/components/contactContainer.jsx
+++ src/components/contactContainer.jsx
@@ -6,6 +6,9 @@ class contactContainer extends Component {
     return (
       <div>
         <h1>Contact ME</h1>
+        <h1>LinkedIn</h1>
+        <h1>Phone Number</h1>
+        <h1>Email</h1>
       </div>
     );
   }
