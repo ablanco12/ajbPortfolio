c319e32813f23928e23a93984cba885a564553ed
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-02-26 18:29:40 -0600

    customizing the navbar and my name

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -18,7 +18,7 @@ const NavBar = props => {
     <div>
       <Navbar color="faded" light>
         <NavbarBrand href="/" className="mr-auto">
-          Andrew-Joseph Blanco
+          ANDREWJOSEPHBLANCO
         </NavbarBrand>
         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
         <Collapse isOpen={!collapsed} navbar>
