81e5c334a4c1643dda1fd45859555b1269a71a48
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-03-02 16:37:26 -0600

    creating the cards for Projects

--- src/components/projectsContainer.jsx
+++ src/components/projectsContainer.jsx
@@ -1,8 +1,77 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 class Projects extends React.Component {
   render() {
-    return <h1>Hello, You</h1>;
+    return (
+      <div>
+        <div>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    );
   }
 }
 
