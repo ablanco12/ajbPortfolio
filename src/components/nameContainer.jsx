d6963aced81af1fecca5f40f9ec0a4f2d5c5aa4c
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-03-15 16:18:04 -0500

    name container push

--- src/components/nameContainer.jsx
+++ src/components/nameContainer.jsx
@@ -8,7 +8,7 @@ const Name = () => {
         text="ANDREWJOSEPHBLANCO"
         effect="color"
         effectChange="#2c435d"
-        delay={1.1}
+        delay={1.2}
         speed={20}
       />
     </div>
