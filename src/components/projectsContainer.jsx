bd50eba33f0883bccff5656c5754bc1ec8b324af
Andrew - Joseph Blanco <44991800+ablanco12@users.noreply.github.com>
2020-03-24 14:01:09 -0500

    adding a div to organize cards

--- src/components/projectsContainer.jsx
+++ src/components/projectsContainer.jsx
@@ -12,60 +12,63 @@ import {
 class Projects extends React.Component {
   render() {
     return (
-      <div className="row">
-        <Card className="column">
-          <CardImg
-            top
-            width="25%"
-            src="/assets/318x180.svg"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>brewPhoria</CardTitle>
-            <CardSubtitle>Beer Tracker</CardSubtitle>
-            <CardText>
-              an application for keeping track of all those beers you've tried!
-            </CardText>
-            <Button>Go</Button>
-          </CardBody>
-        </Card>
+      <div>
+        <div className="row">
+          <Card className="column">
+            <CardImg
+              top
+              width="25%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>brewPhoria</CardTitle>
+              <CardSubtitle>Beer Tracker</CardSubtitle>
+              <CardText>
+                an application for keeping track of all those beers you've
+                tried!
+              </CardText>
+              <Button>Go</Button>
+            </CardBody>
+          </Card>
 
-        <Card className="column">
-          <CardImg
-            top
-            width="25%"
-            src="/assets/318x180.svg"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>thisPacificPlace</CardTitle>
-            <CardSubtitle>traveling to the Philippines ?</CardSubtitle>
-            <CardText>
-              This application gives you a basic rundown of what to do's and
-              eats in the beautiful islands of the Philippines.
-            </CardText>
-            <Button>Go</Button>
-          </CardBody>
-        </Card>
+          <Card className="column">
+            <CardImg
+              top
+              width="25%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>thisPacificPlace</CardTitle>
+              <CardSubtitle>traveling to the Philippines ?</CardSubtitle>
+              <CardText>
+                This application gives you a basic rundown of what to do's and
+                eats in the beautiful islands of the Philippines.
+              </CardText>
+              <Button>Go</Button>
+            </CardBody>
+          </Card>
 
-        <Card className="column">
-          <CardImg
-            top
-            width="25%"
-            src="/assets/318x180.svg"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>PetShop</CardTitle>
-            <CardSubtitle>
-              Dog Grooming business management application
-            </CardSubtitle>
-            <CardText>
-              an application for managing your dog grooming business.
-            </CardText>
-            <Button>Go</Button>
-          </CardBody>
-        </Card>
+          <Card className="column">
+            <CardImg
+              top
+              width="25%"
+              src="/assets/318x180.svg"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>PetShop</CardTitle>
+              <CardSubtitle>
+                Dog Grooming business management application
+              </CardSubtitle>
+              <CardText>
+                an application for managing your dog grooming business.
+              </CardText>
+              <Button>Go</Button>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
   }
